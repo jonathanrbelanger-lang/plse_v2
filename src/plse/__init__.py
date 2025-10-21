3ebd0287957aff99830d8db28f548d3f80164012
Jonathan R Belanger <jonathan.r.belanger@gmail.com>
2025-10-20 20:48:25 -0400

    Initialize plse as a Python package

---
+++ src/plse/__init__.py
@@ -0,0 +1,7 @@
+plse_v2/
+├── patterns/
+│   └── .gitkeep          # An empty file to make the directory appear in git
+├── src/
+│   └── plse/
+│       └── __init__.py   # Makes 'plse' a Python package
+└── pyproject.toml        # Our main project definition file
